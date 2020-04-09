f5ab4bf69b28ca56824794d47d5ebaa437dde0d7
Dani Sánchez <sjp.daniel@gmail.com>
2020-04-09 09:42:11 +0200

    sad
    das

--- LetrasRepetidasCsharp/LetrasRepetidasCsharp/obj/Debug/netcoreapp3.1/LetrasRepetidasCsharp.AssemblyInfo.cs
+++ LetrasRepetidasCsharp/LetrasRepetidasCsharp/obj/Debug/netcoreapp3.1/LetrasRepetidasCsharp.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Letras")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LetrasRepetidasCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Letras")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Letras")]
+[assembly: System.Reflection.AssemblyProductAttribute("LetrasRepetidasCsharp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LetrasRepetidasCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
