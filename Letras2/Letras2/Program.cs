37f6eaa63f07c726595c769b5904aa8fc5836073
Dani Sánchez <sjp.daniel@gmail.com>
2020-04-10 23:08:38 +0200

    Ejercicio
    Muestra letras de una array, indica si es vocal o consonante y el numero de veces que aparece.

---
+++ Letras2/Letras2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Letras2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca su nombre");
+            String nombre;
+            nombre = Console.ReadLine();
+            Console.WriteLine("El nombre introducido es: " + nombre);
+            char[] arr;
+            arr = nombre.ToCharArray();
+            Console.WriteLine("Letra por letra");
+            foreach (char c in arr)
+                Console.WriteLine(c);
+            List<char> list = arr.ToList();
+  
+            int i = 0;
+            // char[] vocales = { 'a', 'e', 'i', 'o', 'u' }; esto no lo he usado al final.
+            foreach (char c in list)
+            {
+
+                char p = list[i];
+                if ((int)char.GetNumericValue(p) >= 0)
+
+                {
+                    Console.WriteLine("Los Nombres de personas no Continenen Numeros");
+                }
+                else
+                {
+                    if (p.Equals('a') | p.Equals('e') | p.Equals('i') | p.Equals('o') | p.Equals('u'))
+                    {
+                        Console.WriteLine("VOCAL");
+                    }
+                    else
+                    {
+                        Console.WriteLine("CONSONANTE");
+                    }
+                }
+                i++;
+            }
+            for (int p = 0; p < arr.Length; p++)
+            {
+                int count = 0;
+                for (int j = 0; j < arr.Length; j++)
+                {
+
+                    if (arr[p] == arr[j])
+                    {
+                        count++;
+                        if (numero(arr[p]))
+                        {
+                            list[p] = arr[p];
+
+                        }
+                    }
+                }
+                if (list[p] != 0)
+                {
+                    if (list[p].Equals('a') | list[p].Equals('e') | list[p].Equals('i') | list[p].Equals('o') | list[p].Equals('u'))
+                    {
+                        Console.WriteLine(list[p] + " se repite: " + count + "(VOCAL)");
+                    }
+                    else
+                    {
+                        Console.WriteLine(list[p] + " se repite: " + count + "(CONSONANTE)");
+                    }
+
+                }
+            }
+            bool numero(int num)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == num)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+        }
+
+
+    }
+
+}
+
