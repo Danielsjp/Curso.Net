4a8c64caad5a003bc16e1f3ecccad0c05d3d3c73
Dani Sánchez <sjp.daniel@gmail.com>
2020-04-13 11:46:11 +0200

    ejercicio finalizado
    final letras repetidas

--- Letras2/Letras2/Program.cs
+++ Letras2/Letras2/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
-
 namespace Letras2
 {
     class Program
@@ -11,6 +9,7 @@ namespace Letras2
         {
             Console.WriteLine("Introduzca su nombre");
             String nombre;
+            String surName;
             nombre = Console.ReadLine();
             Console.WriteLine("El nombre introducido es: " + nombre);
             char[] arr;
@@ -19,15 +18,15 @@ namespace Letras2
             foreach (char c in arr)
                 Console.WriteLine(c);
             List<char> list = arr.ToList();
-  
+            surName = "SANCHEZ";
+            char[] surNamechar;
+            surNamechar = surName.ToCharArray();
+            List<char> SurNC = surNamechar.ToList();
             int i = 0;
-            // char[] vocales = { 'a', 'e', 'i', 'o', 'u' }; esto no lo he usado al final.
             foreach (char c in list)
             {
-
                 char p = list[i];
                 if ((int)char.GetNumericValue(p) >= 0)
-
                 {
                     Console.WriteLine("Los Nombres de personas no Continenen Numeros");
                 }
@@ -35,11 +34,11 @@ namespace Letras2
                 {
                     if (p.Equals('a') | p.Equals('e') | p.Equals('i') | p.Equals('o') | p.Equals('u'))
                     {
-                        Console.WriteLine("VOCAL");
+                       // Console.WriteLine("VOCAL");
                     }
                     else
                     {
-                        Console.WriteLine("CONSONANTE");
+                        // Console.WriteLine("CONSONANTE");
                     }
                 }
                 i++;
@@ -49,28 +48,32 @@ namespace Letras2
                 int count = 0;
                 for (int j = 0; j < arr.Length; j++)
                 {
-
                     if (arr[p] == arr[j])
                     {
                         count++;
                         if (numero(arr[p]))
                         {
                             list[p] = arr[p];
-
                         }
                     }
                 }
                 if (list[p] != 0)
                 {
+                    Dictionary<char, int> letr = new Dictionary<char, int>();
                     if (list[p].Equals('a') | list[p].Equals('e') | list[p].Equals('i') | list[p].Equals('o') | list[p].Equals('u'))
                     {
                         Console.WriteLine(list[p] + " se repite: " + count + "(VOCAL)");
+                        letr.Add(list[p], count);
                     }
                     else
                     {
                         Console.WriteLine(list[p] + " se repite: " + count + "(CONSONANTE)");
+                        letr.Add(list[p], count);
                     }
-
+                    foreach (KeyValuePair<char, int> let in letr)
+                    {
+                        Console.WriteLine(let.Key + "---" + let.Value);
+                    }
                 }
             }
             bool numero(int num)
@@ -85,10 +88,11 @@ namespace Letras2
                 return true;
             }
 
+             List<char> resultado = list.Concat(" ").Concat(SurNC).ToList();
+            
+            Console.WriteLine(resultado[4]);
+            
         }
-
-
     }
-
 }
 
