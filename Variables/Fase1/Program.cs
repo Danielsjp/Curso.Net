2c3388c58deec07feb16114de4946b5e755fb969
Dani Sánchez <sjp.daniel@gmail.com>
2020-04-14 10:37:35 +0200

    ejercicio variables

---
+++ Variables/Fase1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nom, cognom1, cognom2;
+            int dia, mes, any;
+            nom = "Dani";
+            cognom1 = "Sanchez";
+            cognom2 = "Jimenez";
+            dia = 2;
+            mes = 10;
+            any = 1982;
+            var concatnom = cognom1 + " " + cognom2 + " " + nom;
+            string concatdata = dia.ToString() + "/" + mes.ToString() + "/" + any.ToString();
+            Console.WriteLine(concatnom);
+            Console.WriteLine(concatdata);
+        }
+    }
+}
---
+++ Variables/Fase2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nom, cognom1, cognom2;
+            int dia, mes, any;
+            nom = "Dani";
+            cognom1 = "Sanchez";
+            cognom2 = "Jimenez";
+            dia = 2;
+            mes = 10;
+            any = 1982;
+            var concatname = cognom1 + " " + cognom2 + " " + nom;
+            string concatdates = dia.ToString() + "/" + mes.ToString() + "das" + any.ToString();
+            Console.WriteLine(concatname);
+            Console.WriteLine(concatdates);
+        }
+    }
+}
