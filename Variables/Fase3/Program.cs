0056b744e0092fbbcc7823902ac93d9bf1eb52b4
Dani Sánchez <sjp.daniel@gmail.com>
2020-04-14 12:32:02 +0200

    fase 3 OK
    fin

--- Variables/Fase3/Program.cs
+++ Variables/Fase3/Program.cs
@@ -6,7 +6,17 @@ namespace Fase3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int traspas = 1948;
+            int seRepite = 4;
+            int fechaNacim = 1982;
+            bool condicion;
+
+            for (int i = traspas; i<fechaNacim; i=i+1)
+            {            
+                int resto = i % seRepite;
+                if (resto == 0) { condicion = true; string OK = $"L´any {i} es de traspas: {condicion}"; Console.WriteLine(OK); }      
+                else { condicion = false; string KO = $"L´any {i} es de traspas: {condicion}"; Console.WriteLine(KO); }
+            }
         }
     }
 }
