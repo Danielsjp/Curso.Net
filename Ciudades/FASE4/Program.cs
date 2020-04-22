54168e97708b7883ec3bea5d74b1ce4928af761d
Dani Sánchez <sjp.daniel@gmail.com>
2020-04-22 12:33:25 +0200

    ok
    ok

--- Ciudades/FASE4/Program.cs
+++ Ciudades/FASE4/Program.cs
@@ -22,27 +22,33 @@ namespace FASE4
             Console.WriteLine("Introduce Nombre Ciudad 6");
             string ciudad6 = Console.ReadLine();
 
-            char[] ciudadZ = ciudad1.ToCharArray();
+            char[] ciudadZ = new char[ciudad1.Length];
+            ciudadZ = ciudad1.ToCharArray();
             Array.Reverse(ciudadZ);
             Console.WriteLine(string.Join("",ciudadZ));
 
-            char[] ciudadX = ciudad2.ToCharArray();
+            char[] ciudadX = new char[ciudad2.Length];
+            ciudadX = ciudad2.ToCharArray();
             Array.Reverse(ciudadX);
             Console.WriteLine(string.Join("", ciudadX));
 
-            char[] ciudadC = ciudad3.ToCharArray();
+            char[] ciudadC = new char[ciudad3.Length];
+            ciudadC = ciudad3.ToCharArray();
             Array.Reverse(ciudadC);
             Console.WriteLine(string.Join("", ciudadC));
 
-            char[] ciudadV = ciudad4.ToCharArray();
+            char[] ciudadV = new char[ciudad4.Length];
+            ciudadV = ciudad4.ToCharArray();
             Array.Reverse(ciudadV);
             Console.WriteLine(string.Join("", ciudadV));
-            
-            char[] ciudadB = ciudad5.ToCharArray();
+
+            char[] ciudadB = new char[ciudad5.Length];
+            ciudadB = ciudad5.ToCharArray();
             Array.Reverse(ciudadB);
             Console.WriteLine(string.Join("", ciudadB));
 
-            char[] ciudadN = ciudad6.ToCharArray();
+            char[] ciudadN = new char[ciudad6.Length];
+            ciudadN = ciudad6.ToCharArray();
             Array.Reverse(ciudadN);
             Console.WriteLine(string.Join("", ciudadN));
 
