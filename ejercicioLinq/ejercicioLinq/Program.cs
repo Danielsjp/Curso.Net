03e33166669ec1aa5410b81150589ca4abff5565
Dani Sánchez <sjp.daniel@gmail.com>
2020-06-17 08:56:50 +0200

    proyecto vacio
    proyecto vacio

---
+++ ejercicioLinq/ejercicioLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejercicioLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
